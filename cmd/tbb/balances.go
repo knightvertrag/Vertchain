58e24c5289e738b32dd0e2d47240f0586edab0ba
knightvertrag <amiapu1@gmail.com>
2021-07-27 01:33:17 +0530

    Added subcommand to list balances

---
+++ cmd/tbb/balances.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"vertchain.com/tbb/database"
+)
+
+func balancesCmd() *cobra.Command {
+	var balancesCmd = &cobra.Command{
+		Use:   "balances",
+		Short: "Interact with balances (list...).",
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			return incorrectUsageError()
+		},
+		Run: func(cmd *cobra.Command, args []string) {},
+	}
+
+	balancesCmd.AddCommand(balancesListCmd())
+	return balancesCmd
+}
+
+func balancesListCmd() *cobra.Command {
+	var balancesListCmd = &cobra.Command{
+		Use:   "list",
+		Short: "Lists all balances",
+		Run: func(cmd *cobra.Command, args []string) {
+			state, err := database.NewStateFromDisk()
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+			defer state.Close()
+
+			fmt.Println("Accounts balances:")
+			fmt.Println("_______________")
+			fmt.Println("")
+			for account, balance := range state.Balances {
+				fmt.Printf("%s: %d\n", account, balance)
+			}
+		},
+	}
+	return balancesListCmd
+}
